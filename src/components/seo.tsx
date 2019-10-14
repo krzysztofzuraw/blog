d4446902df1fbfa4032ba99f719cc02f3f9fe61f
Krzysztof Żuraw <krzysztofzuraw@fastmail.com>
2019-10-14 18:46:13 +0200

    fix: import

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,6 +1,6 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import * as React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 
 function SEO({
   description,
