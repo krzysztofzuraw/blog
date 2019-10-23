cd8d393ad46ed677fbe4bdbf430d4ee9b4fb438e
Krzysztof Żuraw <krzysztofzuraw@fastmail.com>
2019-10-23 17:42:12 +0200

    2019-10-23 update

--- src/pages/learn-in-public.tsx
+++ src/pages/learn-in-public.tsx
@@ -14,20 +14,35 @@ const LearnInPublicPage: React.FC<Props> = ({ location }) => (
 
     <div>
       <p>Inspired by learn in public blog post I decided to create my own page for that purpose.</p>
-      <p>Mindframing:</p>
-      <ul>
-        <li>Pact: I decicate time (1h15min x5 per week) to make progess in learning in public</li>
-        <li>Things I'm currently learning in public: </li>
-        <li>TypeScript</li>
-        <li>React</li>
-        <p>
-          My sucess critera: help others by writing 52 articles (either blog & today I Learned
-          entries) from TypeScript + React; have impact on one person
-        </p>
-        <p>Time spent progress:</p>
-        <li>Act: </li>
-        https://nesslabs.com/mindframing
-      </ul>
+      <p>
+        I picked <a href="https://nesslabs.com/mindframing">mindframing</a> framework.
+      </p>
+      <ol>
+        <li>
+          <p>Pact: </p>
+          <p>I decicate time (1h15min x5 per week) to make progess in learning in public</p>
+          <p>What I'm currently learning in public:</p>
+          <ul>
+            <li>TypeScript</li>
+            <li>React</li>
+          </ul>
+          <p>My sucess critera:</p>
+          <ul>
+            <li>
+              help others by writing 52 articles (either blog and today I learned entries) from
+              TypeScript + React (0/52)
+            </li>
+            <li>have impact on one person (0/1)</li>
+          </ul>
+          <p>Time spent progress: </p>
+        </li>
+        <li>
+          <p>Act and react</p>
+        </li>
+        <li>
+          <p>Impact</p>
+        </li>
+      </ol>
     </div>
   </Layout>
 );
