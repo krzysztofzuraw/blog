c272bdcc6f5e44b8c9a338d0a0ffd757c28eb8e4
Krzysztof Żuraw <services@kzuraw.com>
2020-02-04 18:25:58 +0100

    chore: next update

--- src/pages/microblog.tsx
+++ src/pages/microblog.tsx
@@ -1,14 +1,36 @@
 import { graphql } from 'gatsby';
 import * as React from 'react';
 
+import { MicroblogIndexPageQuery } from 'typings/graphql';
 import Layout from '../components/layout';
+import Link from '../components/link';
 import SEO from '../components/seo';
 
-const MicroblogIndex: React.FunctionComponent = () => {
+import style from '../styles/micropost.module.css';
+
+type Props = {
+  data: MicroblogIndexPageQuery;
+};
+
+const MicroblogIndex: React.FunctionComponent<Props> = ({ data }) => {
+  const posts = data.allMarkdownRemark.edges;
   return (
     <Layout>
       <SEO title="Microblog" />
-      <div>Working</div>
+      {posts.map(post => (
+        <div key={post.node.id} className={style.entry} id={post.node.frontmatter?.title ?? ''}>
+          <h1 className={`${style.title} post-link`}>
+            <Link to={`#${post.node.frontmatter?.title}`}>{post.node.frontmatter?.title}</Link>
+          </h1>
+          <div className={style.meta}>{post.node.frontmatter?.date}</div>
+          <div dangerouslySetInnerHTML={{ __html: post.node.html ?? '' }} />
+          {post.node.frontmatter?.twitter && (
+            <div className={style.meta}>
+              <a href={post.node.frontmatter?.twitter}>Twitter link</a>
+            </div>
+          )}
+        </div>
+      ))}
     </Layout>
   );
 };
@@ -24,7 +46,7 @@ export const pageQuery = graphql`
       edges {
         node {
           id
-          excerpt
+          html
           frontmatter {
             title
             date(formatString: "MMMM DD, YYYY")
