b9423a6dfdf61a0673b78d81a6fa8d11210a79d7
Krzysztof Żuraw <services@kzuraw.com>
2020-02-28 18:11:34 +0100

    fix: netlify builds

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,12 @@
-export const parseDate = (dateString: string) =>
-  new Date(dateString).toLocaleString(navigator.language, {
+export const parseDate = (dateString: string) => {
+  let locale;
+  // check if navigator is defined for gatsby production build to pass
+  if (typeof window !== 'undefined') {
+    locale = navigator.language;
+  }
+  return new Date(dateString).toLocaleString(locale, {
     year: 'numeric',
     month: 'numeric',
     day: 'numeric',
   });
+};
