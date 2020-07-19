bc8b1e44f3bbc9c9e16914660a01984eed7da3c8
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2020-07-19 13:04:59 +0200

    chore: updates (#406)

--- src/pages/now.tsx
+++ src/pages/now.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react';
-import { Layout, Link, SEO } from '../components';
+import { Layout, SEO } from '../components';
 
 const NowPage: React.FunctionComponent = () => {
   return (
     <Layout>
       <SEO title="Now | Krzysztof Żuraw" description="What I'm doing now" slug="/now" />
-      <h2>Coding</h2>
-      <p>
-        Moving back to <Link to="https://www.ingrid.com">Ingrid</Link> office. Getting back to side
-        projects & writing. Co-organizing&nbsp;
-        <Link to="https://www.meetup.com/pl-PL/WrocTypeScript/">Wrocław TypeScript</Link>
-      </p>
-      <h2>Free time</h2>
-      <p>
-        Trying zettelkasten & beeminder. Moving to plain text for day plans & notes. Doing{' '}
-        <Link to="https://gmb.io/is/">GMB integral strength</Link>.
-      </p>
-      <p>Last updated - 2020-07-01</p>
+      <ul>
+        <li>Living in Wrocław</li>
+        <li>Started running - again.</li>
+        <li>Started doing some experiments on CodePen.</li>
+        <li>Moved to plain text for wiki & todos</li>
+      </ul>
+      <p>Last updated - 2020-07-19</p>
     </Layout>
   );
 };
