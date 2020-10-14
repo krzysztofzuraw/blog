cfe79efaeb58183e0e4e7d9fb16c97af35c0dd85
Krzysztof Żuraw <services@kzuraw.com>
2020-10-14 18:31:43 +0200

    chore: updates

--- src/pages/uses.tsx
+++ src/pages/uses.tsx
@@ -8,14 +8,14 @@ const UsesPage: FunctionComponent = () => {
       <Stack>
         <h1>Tools that I'm using</h1>
         <p>
-          I spend too much time luring through other people tooling and too little time spend on
+          I spend too much time lurking through other people tooling and too little time spend on
           doing actual work. So I decided to have my <Link to="https://uses.tech/">uses page</Link>{' '}
           a little bit different.
         </p>
         <p>
           I use <Link to="https://code.visualstudio.com/Download">code editor</Link> &{' '}
-          <Link to="https://www.mozilla.org/en-US/firefox/developer/">browser</Link>. I plan my day
-          in plain text and calendar. I'm using Apple products. I brew coffee in Chemex or V60.
+          <Link to="https://www.google.com/chrome/">browser</Link>. I plan my day in plain text and
+          calendar. I'm using Apple products. I brew coffee in Chemex or V60.
         </p>
         <p>
           Last updated on <time>September 25, 2020</time>
