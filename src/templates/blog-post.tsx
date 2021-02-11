892c8984cef6873e73d809d080c6326841bdf65c
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2021-02-11 14:26:00 +0100

    feat: initial version (#491)

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -18,6 +18,35 @@ const BlogPostPage: React.FunctionComponent<Props> = ({ data: { markdownRemark }
         <time>Published on {markdownRemark.frontmatter.date}</time>
         <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
       </article>
+      <hr />
+      <form
+        className="newsletter"
+        action="https://buttondown.email/api/emails/embed-subscribe/krzysztof_zuraw"
+        method="post"
+        target="popupwindow"
+        onSubmit={() => {
+          window.open('https://buttondown.email/krzysztof_zuraw', 'popupwindow');
+        }}
+      >
+        <h3 className="newsletter-header">
+          RSS is not your thing? Subscribe to monthly newsletter istead!
+        </h3>
+        <div className="newsletter-content">
+          <div className="input-wrapper">
+            <label htmlFor="email">Enter your email</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <input type="hidden" value="1" name="embed" />
+          <input type="submit" className="submit-button" value="Subscribe" />
+        </div>
+        <p className="credits">
+          Powered by{' '}
+          <a href="https://buttondown.email" target="_blank" rel="noreferrer">
+            Buttondown
+          </a>
+          .
+        </p>
+      </form>
     </Layout>
   );
 };
