2de7dcfee8af38529352752c74e1934a68a4a850
Krzysztof Żuraw <services@kzuraw.com>
2021-03-18 08:06:21 +0100

    feat: add basic animation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,21 @@
+import { AnimatePresence } from 'framer-motion';
+import React from 'react';
 import './src/styles/global.css';
+
+export const wrapPageElement = ({ element }) => <AnimatePresence>{element}</AnimatePresence>;
+
+export const shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPosition }) => {
+  // transition duration from `layout.tsx` * 1000 to get time in ms
+  // * 2 for exit + enter animation
+  const TRANSITION_DELAY = 0.3 * 1000 * 2;
+  // if it's a "normal" route
+  if (location.action === 'PUSH') {
+    window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY);
+  }
+  // if we used the browser's forwards or back button
+  else {
+    const savedPosition = getSavedScrollPosition(location) || [0, 0];
+    window.setTimeout(() => window.scrollTo(...savedPosition), TRANSITION_DELAY);
+  }
+  return false;
+};
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React, { FunctionComponent } from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from './link';
@@ -39,7 +40,14 @@ export const Layout: FunctionComponent = ({ children }) => {
             </li>
           </ul>
         </header>
-        <main>{children}</main>
+        <motion.main
+          initial={{ opacity: 0, x: -200 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 200 }}
+          transition={{ type: 'spring', mass: 0.35, stiffness: 75, duration: 0.3 }}
+        >
+          <main>{children}</main>
+        </motion.main>
         <footer role="contentinfo">
           <ul>
             <li>
