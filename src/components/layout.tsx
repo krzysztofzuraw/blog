668af00014badb70a12883aeaf43ea024897d82a
Krzysztof Żuraw <services@kzuraw.com>
2021-03-27 09:38:15 +0100

    feat: migrate index page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,44 +3,47 @@ import { Link } from './link';
 
 export const Layout: FunctionComponent = ({ children }) => {
   return (
-    <div className="mx-auto max-w-3xl grid h-screen">
-      <header role="banner">
-        <ul>
-          <li>
+    <div className="mx-auto max-w-3xl grid h-screen p-4 gap-6">
+      <header role="banner" className="grid place-items-end">
+        <ul className="grid grid-cols-2 gap-2 font-bold text-lg ">
+          <li className="hover:underline">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="hover:underline">
             <Link to="/blog">Blog</Link>
           </li>
         </ul>
       </header>
       {children}
-      <footer role="contentinfo">
+      <footer
+        role="contentinfo"
+        className="grid text-lg grid-cols-3 border-gray-200 border-t-2 py-2"
+      >
         <ul>
-          <li>
+          <li className="hover:underline">
             <Link to="/uses">Uses</Link>
           </li>
-          <li>
+          <li className="hover:underline">
             <Link to="/now">Now</Link>
           </li>
-          <li>
+          <li className="hover:underline">
             <Link to="https://buttondown.email/krzysztof_zuraw">Newsletter</Link>
           </li>
-          <li>
+          <li className="hover:underline">
             <Link to="/credits">Credits</Link>
           </li>
-          <li>
+          <li className="hover:underline">
             <Link to="/feeds/all.rss.xml">RSS</Link>
           </li>
         </ul>
         <ul className="social">
-          <li>
+          <li className="hover:underline">
             <Link to="https://github.com/krzysztofzuraw">GitHub</Link>
           </li>
-          <li>
+          <li className="hover:underline">
             <Link to="https://twitter.com/krzysztof_zuraw">Twitter</Link>
           </li>
-          <li>
+          <li className="hover:underline">
             <Link to="https://pl.linkedin.com/in/krzysztofzuraw">LinkedIn</Link>
           </li>
         </ul>
