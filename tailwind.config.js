743bde563047ddb4661f192639afd8ded9dae834
Krzysztof Żuraw <services@kzuraw.com>
2022-02-08 21:06:42 +0100

    feat: use custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js,md,xml}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      mono: ["MonoLisa", "monospace"],
+    },
     extend: {
       gridTemplateRows: {
         layout: "auto 1fr auto",
