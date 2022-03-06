62a4c995ef5f010663ccc4bd31e243d85d094d0c
Krzysztof Żuraw <services@kzuraw.com>
2022-03-06 12:46:56 +0100

    feat: add new shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -64,6 +64,31 @@ module.exports = (config) => {
     </figure>`;
   });
 
+  config.addShortcode("rateToWords", (rate) => {
+    switch (rate) {
+      case 1:
+        return "misunderstanding";
+      case 2:
+        return "very bad";
+      case 3:
+        return "bad";
+      case 4:
+        return "okeish";
+      case 5:
+        return "average";
+      case 6:
+        return "not bad";
+      case 7:
+        return "good";
+      case 8:
+        return "very good";
+      case 9:
+        return "sensational";
+      case 10:
+        return "masterpiece";
+    }
+  });
+
   return {
     templateFormats: ["md", "njk", "html"],
     htmlTemplateEngine: "njk",
