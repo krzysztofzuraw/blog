98bcb5aa3a2b116d2e326335d1bf2d0623eed492
Krzysztof Żuraw <services@kzuraw.com>
2022-03-12 13:31:21 +0100

    feat: use default fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   content: ["./src/**/*.{html,js,md,xml}"],
   theme: {
     extend: {
       gridTemplateRows: {
         layout: "auto 1fr auto",
-      },
-      fontFamily: {
-        serif: ["Charter", ...defaultTheme.fontFamily.serif],
-        mono: ["MonoLisa", ...defaultTheme.fontFamily.mono],
       },
     },
   },
