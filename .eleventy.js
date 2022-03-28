9acfafc44e5bcbb44d4bafacc851461917bd846b
Krzysztof Żuraw <services@kzuraw.com>
2022-03-28 18:22:19 +0200

    feat: cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,6 @@ const { DateTime } = require("luxon");
 const sizeOf = require("image-size");
 const path = require("path");
 
-require("dotenv").config();
-
 module.exports = (config) => {
   config.addPassthroughCopy("src/img");
   config.addPassthroughCopy({ "src/passthrough": "/" });
@@ -25,21 +23,13 @@ module.exports = (config) => {
     });
   });
 
-  config.addFilter("getLanguage", (lang) => {
-    switch (lang) {
-      case "en":
-        return "English";
-      case "pl":
-        return "Polish";
-      default:
-        return "English";
-    }
-  });
-
-  config.addFilter("formatDate", (date) => DateTime.fromJSDate(date).toFormat("yyyy-MM-dd"));
-
-  config.addFilter("formatISO", (dateString) =>
-    DateTime.fromISO(dateString).toFormat("yyyy-MM-dd")
+  config.addFilter("formatDate", (date) =>
+    DateTime.fromJSDate(date).toLocaleString({
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      timeZone: "Europe/Warsaw",
+    })
   );
 
   config.addFilter("toISO", (date) => DateTime.fromJSDate(date, { zone: "Europe/Warsaw" }).toISO());
@@ -59,31 +49,6 @@ module.exports = (config) => {
       </a>
       <figcaption>${figcaption}</figcaption>
     </figure>`;
-  });
-
-  config.addShortcode("rateToWords", (rate) => {
-    switch (rate) {
-      case 1:
-        return "misunderstanding";
-      case 2:
-        return "very bad";
-      case 3:
-        return "bad";
-      case 4:
-        return "okeish";
-      case 5:
-        return "average";
-      case 6:
-        return "not bad";
-      case 7:
-        return "good";
-      case 8:
-        return "very good";
-      case 9:
-        return "sensational";
-      case 10:
-        return "masterpiece";
-    }
   });
 
   return {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   content: ["./src/**/*.{html,js,md,xml}"],
   theme: {
     extend: {
-      fontFamily: {
-        serif: ["Charter", ...defaultTheme.fontFamily.serif],
-        mono: ["MonoLisa", ...defaultTheme.fontFamily.mono],
-      },
       gridTemplateRows: {
         layout: "auto 1fr auto",
-      },
-      gridTemplateColumns: {
-        bookmarks: "1fr auto",
       },
     },
   },
