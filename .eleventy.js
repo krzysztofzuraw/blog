63f542e8b37c6a11135866e44d04d867a43a3b52
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2022-07-26 15:24:57 +0200

    feat: add reading (#587)
    * feat: add reading
    
    * fix: date

--- .eleventy.js
+++ .eleventy.js
@@ -23,6 +23,17 @@ module.exports = (config) => {
     });
   });
 
+  config.addFilter("getLanguage", (lang) => {
+    switch (lang) {
+      case "en":
+        return "English";
+      case "pl":
+        return "Polish";
+      default:
+        return "English";
+    }
+  });
+
   config.addFilter("formatDate", (date) => DateTime.fromJSDate(date).toFormat("yyyy-MM-dd"));
 
   config.addFilter("toISO", (date) => DateTime.fromJSDate(date, { zone: "Europe/Warsaw" }).toISO());
@@ -48,9 +59,35 @@ module.exports = (config) => {
     </figure>`;
   });
 
+  config.addShortcode("rateToWords", (rate) => {
+    switch (rate) {
+      case 1:
+        return "misunderstanding";
+      case 2:
+        return "very bad";
+      case 3:
+        return "bad";
+      case 4:
+        return "ok";
+      case 5:
+        return "average";
+      case 6:
+        return "not bad";
+      case 7:
+        return "good";
+      case 8:
+        return "very good";
+      case 9:
+        return "sensational";
+      case 10:
+        return "masterpiece";
+    }
+  });
+
   config.addLayoutAlias("base", "layouts/base.html");
   config.addLayoutAlias("page", "layouts/page.html");
   config.addLayoutAlias("post", "layouts/post.html");
+  config.addLayoutAlias("book", "layouts/book.html");
 
   return {
     templateFormats: ["md", "njk", "html"],
---
+++ src/reading/2021-08-09-undeath-and-taxes.md
@@ -0,0 +1,16 @@
+---
+title: Undeath and Taxes
+date: 2021-08-09
+author: Drew Hayes
+rating: 7
+lang: en
+publisher: REUTS Publications
+year: 2015
+isbn: B012P8CF6Q
+---
+
+Second part of Fred the Vampire Accountant series with more stories about parahuman side of life. As this is a second part of series I was little worried that this book won't be as good as the first one. To my relief it wasn't the case for this one.
+
+I liked a plot & stories that was presented in the book - it was fun summer read.
+
+Book can be found on [Amazon](https://www.amazon.com/Undeath-Taxes-Fred-Book-2-ebook/dp/B012P8CF6Q).
