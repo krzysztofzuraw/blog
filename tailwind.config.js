65f9796bf182f78d2b1fe2e7bbb451a0b7dea260
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2022-08-30 14:20:25 +0200

    feat: remove tailwind

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,md}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
