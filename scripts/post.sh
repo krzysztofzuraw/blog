5c01602012e685768ebd750a87d9814f18e35247
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-06-08 12:57:01 +0200

    feat: bring back tools

---
+++ scripts/post.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash	
+
+echo "---	
+spell-checker: disable	
+title: WIP	
+description:	
+pubDate: $(date -u +%Y-%m-%dT%H:%M:%SZ)	
+slug: 2023/wip	
+---	
+" > src/content/blog/$(date +%F)-wip.mdx	
+
+code src/content/blog/$(date +%F)-wip.mdx	
+
+echo 'Blog post created'
