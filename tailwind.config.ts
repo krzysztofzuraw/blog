80eb86de387eca996414114edf1639145aa3c885
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-07-02 14:12:31 +0200

    feat: june update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
@@ -26,4 +27,4 @@ module.exports = {
     },
   },
   plugins: [require("@tailwindcss/typography")],
-};
+} satisfies Config;
