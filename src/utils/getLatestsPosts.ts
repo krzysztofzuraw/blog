7cfbee638a51bef62246898b742940a2ef177420
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-07-28 17:18:36 +0200

    Fix lint errors

--- src/utils/getLatestsPosts.ts
+++ src/utils/getLatestsPosts.ts
@@ -3,7 +3,7 @@ import { chain } from "lodash-es";
 
 export const getLatestsPosts = async () =>
   chain(await getCollection("blog"))
-    .sortBy((post) => post.data.pubDate.valueOf())
+    .sortBy((post) => post.data.pubDate?.valueOf())
     .reverse()
     .slice(0, 3)
     .value();
